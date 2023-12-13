06b18477e6e7e53f6959f8d98dc3afb1c0c19fc5
VittoRose <135705006+VittoRose@users.noreply.github.com>
2023-12-13 08:02:17 +0100

    Simbolic F matrix

---
+++ Get_F_matrix.m
@@ -0,0 +1,30 @@
+%% Evaluate the F matrix for EKF
+
+clc
+
+N = param.N;
+M = param.M;
+Rm = param.Rm;
+g = param.g;
+W = param.W;
+P = param.P;
+
+syms xc dxc theta dtheta V;
+q = [xc, dxc, theta, dtheta];
+
+% First row
+a = jacobian(dxc*dt + xc, q);
+
+% Second row
+b = jacobian(dtheta*dt + theta, q);
+
+% Third row
+c = jacobian((V*N*M - dxc*N*W^2 + cos(theta)*g*sin(theta)*Rm*P^2 + dtheta^2*sin(theta)*Rm*N*P)/(Rm*(N*M - cos(theta)^2*P^2))*dt + dxc,q);
+
+% Forth row
+d = jacobian(-P/Rm*(g*sin(theta)*Rm*M + cos(theta)*dtheta^2*sin(theta)*Rm*P + cos(theta)*V*M - cos(theta)*dxc*W^2)/(N*M - cos(theta)^2*P^2)*dt + dtheta, q);
+
+F = [a; b; c; d];
+
+matlabFunction(F,'file','functions\get_F_matrix', 'Vars', {q,V});
+    
--- init_param.m
+++ init_param.m
@@ -61,4 +61,6 @@ Ts.distance_sensor = 1e-2;
 Ts.a1_sensor = 5e-2;
 Ts.angularspeed_sensor = 1e-3;
 
+%% Evaluate the F matrix for EKF
 
+get_F_matrix
