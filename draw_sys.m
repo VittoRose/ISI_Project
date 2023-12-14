49861e036e76ebcca16a0b77f5113752832f974d
VittoRose <135705006+VittoRose@users.noreply.github.com>
2023-12-14 11:10:06 +0100

    EKF analysis and plot

--- draw_sys.m
+++ draw_sys.m
@@ -1,4 +1,4 @@
-function draw_sys(xc, theta, dim)
+function draw_sys(xc, theta, xce, thetae, dim)
 % Function used to draw the system in a specific configuration 
 % xc and theta are the two Lagrangian variable used to describe the system
 % dim is defined in init_param.m
@@ -10,7 +10,6 @@ if xc > dim.xmax
 elseif xc < dim.xmin
     error("Xc exceeded minimum value");
 end
-
 
 hold on
 title("Pendulum motion");
@@ -32,23 +31,31 @@ plot([dim.xmax, dim.xmax], [1, -1], '-b');
 
 % Define cart
 kart = [xc-dim.b/2, -dim.h/2, dim.b, dim.h];
+karte = [xce-dim.b/2, -dim.h/2, dim.b, dim.h];
 
 % Draw kart
 rectangle('Position', kart, 'EdgeColor', 'k', 'LineWidth', 2)
+rectangle('Position', karte, 'EdgeColor', 'r', 'LineWidth', 1)
 
 % Penulum position 
 P_end1 = [xc + 2*dim.L1*sin(theta), 2*dim.L1*cos(theta)];
 P_end2 = [xc - 2*dim.L2*sin(theta), -2*dim.L2*cos(theta)];
+P_end1e = [xce + 2*dim.L1*sin(thetae), 2*dim.L1*cos(thetae)];
+P_end2e = [xce - 2*dim.L2*sin(thetae), -2*dim.L2*cos(thetae)];
 
 % Draw pendulum
 plot([xc, P_end1(1)], [0, P_end1(2)], '-k', 'LineWidth',2);
 plot([xc, P_end2(1)], [0, P_end2(2)], '-k', 'LineWidth',2);
+plot([xce, P_end1e(1)], [0, P_end1e(2)], '-r', 'LineWidth',1);
+plot([xce, P_end2e(1)], [0, P_end2e(2)], '-r', 'LineWidth',1);
 
 % Mass position
 P_mass = [xc - dim.Lp*sin(theta), -dim.Lp*cos(theta)];
+P_masse = [xce - dim.Lp*sin(thetae), -dim.Lp*cos(thetae)];
 
 % Draw mass
 plot(P_mass(1), P_mass(2), 'ko', 'LineWidth',3);
+plot(P_masse(1), P_masse(2), 'ro', 'LineWidth',2);
 
 hold off
 
