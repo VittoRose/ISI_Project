454e281e9fd8bafe8b4202c7295af323669cd030
FP98 <131855491+FP98@users.noreply.github.com>
2023-12-28 10:54:43 +0100

    x_new and x_now format homogenization in RTS algorithm

--- RTS_regolarization.m
+++ RTS_regolarization.m
@@ -1,22 +1,26 @@
 % Rauch Tung Striebel regolarization
 
+% x format homogenization
+x_new = transpose(out.x_new.data);
+x_now = [out.x_EKF.data; out.theta_EKF.data; out.dx_EKF.data; out.dtheta_EKF.data];
+
 % Initialization
 C = zeros(4, 4);
-xs = zeros(4, size(out.x_now.data, 3));
-Ps = zeros(4, 4, size(out.x_now.data, 3));
+xs = zeros(4, length(out.tout));
+Ps = zeros(4, 4, length(out.tout));
 
 % For cicle initialization
-x = out.x_now.data(:, size(out.x_now.data, 3));
-xs(:, size(out.x_now.data, 3)) = x;
+x = x_now(:, length(out.tout));
+xs(:, length(out.tout)) = x;
 
-for i = (size(out.x_now.data, 3) - 1):-1:1
+for i = (length(out.tout) - 1):-1:1
     P = out.P.data(:,:,i);
     P_new = out.P_new.data(:,:,i);
     F = out.F.data(:,:,i);
-    x_now = out.x_now.data(:,i);
-    x_new = out.x_new.data(:,i);
+    x_now1 = x_now(:,i);
+    x_new1 = x_new(:,i);
     
     C = P*transpose(F)*pinv(P_new);
-    x = x_now + C*( x - x_new );
+    x = x_now1 + C*( x - x_new1 );
     xs(:,i) = x;
 end
